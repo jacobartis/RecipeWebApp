bc766bbc2ccac7bd292fb730c685cbe34559f5d4
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-01-05 17:39:28 +0000

    Created skeleton login page.

---
+++ RecipeApp/Pages/Login.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RecipeApp.Pages{
+    public class LoginModel: PageModel{
+        private readonly ILogger<LoginModel> _logger;
+
+        public LoginModel(ILogger<LoginModel> logger){
+            _logger = logger;
+        }
+
+        public void OnGet(){
+            
+        }
+    }
+}
