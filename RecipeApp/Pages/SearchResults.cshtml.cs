167b21dadb05af0b7f13631dd40bb612dd40ec41
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-01-05 17:20:39 +0000

    Search terms now passed to the search results page get call.

--- RecipeApp/Pages/SearchResults.cshtml.cs
+++ RecipeApp/Pages/SearchResults.cshtml.cs
@@ -4,13 +4,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace RecipeApp.Pages{
     public class SearchResultsModel: PageModel{
         private readonly ILogger<SearchResultsModel> _logger;
-
+        public String? SearchInput;
         public SearchResultsModel(ILogger<SearchResultsModel> logger){
             _logger = logger;
         }
 
-        public void OnGet(){
-            
+        public void OnGet(String searchInput){
+            SearchInput = searchInput;
         }
     }
 }
