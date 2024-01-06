591690acc91375f5960eb002ebe0bfefd31991f4
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-01-06 15:46:56 +0000

    Recipe page now trys to find the recipe of the given id, returns page not found if recipe is invalid.

--- RecipeApp/Pages/RecipePage.cshtml.cs
+++ RecipeApp/Pages/RecipePage.cshtml.cs
@@ -1,16 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RecipeApp.Models;
+using RecipeApp.Services;
 
 namespace RecipeApp.Pages{
     public class RecipePageModel: PageModel{
-        private readonly ILogger<RecipePageModel> _logger;
-        public String? Recipe;
-        public RecipePageModel(ILogger<RecipePageModel> logger){
-            _logger = logger;
+        private readonly RecipeService _service;
+        public Recipe? Recipe;
+        public RecipePageModel(RecipeService service){
+            _service = service;
         }
 
-        public void OnGet(String recipe){
-            Recipe = recipe;
+        public IActionResult OnGet(int recipeId){
+            Recipe = _service.GetRecipeById(recipeId);
+            if (Recipe == null){
+                return NotFound();
+            }
+            return Page();
         }
     }
 }
