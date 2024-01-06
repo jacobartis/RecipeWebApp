8f8fe7b3febf3e63728d674382289e3ac5dfa9f9
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-01-06 14:34:29 +0000

    Added new db context and service.

--- RecipeApp/Program.cs
+++ RecipeApp/Program.cs
@@ -1,8 +1,13 @@
-
+using Microsoft.EntityFrameworkCore;
+using RecipeApp.Data;
+using RecipeApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RecipeContext>(options => 
+    options.UseSqlite("Data Source=RecipeDb.db"));
+builder.Services.AddScoped<RecipeService>();
 
 var app = builder.Build();
 
